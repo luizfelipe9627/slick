a79ad98dffbbff6992cd1a8370b63d4b26287135
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2022-11-15 19:32:26 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$('.content-list').slick({
+  // Usado para falar quando slides irão aparecer na tela.
+  slidesToShow: 2,
+  // Usado para falar de quanto em quanto slide deve passar quando clicar nos controles.
+  slidesToScroll: 1,
+  // Permite o autoplay dos slides.
+  autoplay: true,
+  // Tempo definido para ficar passando os slides.
+  autoplaySpeed: 2000,   
+  // Usado para criar as bolinhas de navegação.
+  dots: true,
+});
+
